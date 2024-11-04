41f28a69f8a46979c28c4120e08c98687a4c7e0c
FloWuenne <flowuenne@gmail.com>
2024-11-03 20:51:56 -0500

    Removed encoding script.

--- assets/encode_url_base64.py
+++
@@ -1,9 +0,0 @@
-import base64
-
-# String to encode
-text = "https://docs.google.com/forms/u/0/d/e/1FAIpQLScYvVDqRnRS3LfQMmz2yvt2sfGUbRhBevserv-0-67CnzKqMA/formResponse"
-
-# Encode to base64
-encoded = base64.b64encode(bytes(text, 'utf-8'))
-
-print(f"Base64 encoded string: {encoded}")
